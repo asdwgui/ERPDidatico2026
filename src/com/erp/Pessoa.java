dd95350047452acbc880c4240505d1237b9902d7
Eduardo Sampaio <147956810+sampaioalveseduardo@users.noreply.github.com>
2026-09-06 15:48:15 -0300

    Update Pessoa.java

--- src/com/erp/Pessoa.java
+++ src/com/erp/Pessoa.java
@@ -1,15 +1,18 @@
 package com.erp;
+
 public class Pessoa {
     private String id;
     private int tipo; // 1 - Cliente, 2 - Fornecedor, 3 - Funcionario
     private String nome;
     private String baseLegal; // NOVO: LGPD
+
     public Pessoa(String id, int tipo, String nome, String baseLegal) {
         this.id = id;
         this.tipo = tipo;
         this.nome = nome;
         this.baseLegal = baseLegal;
     }
+
     public String getId() { return id; }
     public int getTipo() { return tipo; }
     public String getNome() { return nome; }
@@ -17,10 +20,12 @@ public class Pessoa {
     public void anonimizar() {
         this.nome = "ANONIMIZADO";
     }
+
     @Override
     public String toString() {
         return id + "," + tipo + "," + nome + "," + baseLegal;
     }
+
     public static Pessoa fromString(String str) {
         String[] parts = str.split(",");
         String baseLegal = parts.length >= 4 ? parts[3] : "Não informado";
